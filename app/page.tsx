e56b8b23887774ac39cfc35e76b6837c5dc76206
Lakshay <lakshay.sbp@gmail.com>
2023-11-23 15:01:46 +0530

    MAde the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
+import Hero from './components/Hero'
+import Newest from './components/Newest'
 
 export default function Home() {
   return (
     //lets start
-    <main>
-     <div>
-      Hello There
-     </div>
-    </main>
+    <div className='bg-white pb-6 sm:pb-8 lg:pb-12'>
+    <Hero/>
+    <Newest/>
+    </div>
   )
 }
